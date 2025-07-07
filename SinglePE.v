22ea655ed84f7b652f74761734e07168e1192199
ttkkkwon <139191744+ttkkkwon@users.noreply.github.com>
2025-07-07 21:56:36 +0900

    Update SinglePE.v

--- SinglePE.v
+++ SinglePE.v
@@ -1,33 +1,39 @@
-module SinglePE (
-    input            clk,
-    input            rst,
-    input            sel,
-    input      [7:0] in_a,
-    input      [7:0] in_b,
-    output     [7:0] out_a,
-    output     [7:0] out
+module SinglePEv1_comb(
+    input clk, rst,
+    input  [7:0] in_a,
+    input  [7:0] in_b,
+    input  [7:0] init,
+    output reg [7:0] out_a,
+    output reg [7:0] out_down
 );
-    wire [7:0] mult_result, out_sum, q_out_c;
-    wire     [7:0] out_b,
-    wire     [7:0] out_c,
+    // 1) Multiplier 인스턴스
+    wire [15:0] mult_w;
+    Multiplier Mult1 (
+        .clk(clk), .rst(rst),
+        .a(in_a),
+        .b(init),
+        .out(mult_w)
+    );
 
-    if(rst == 1'b1) assign q_out_c = 8'b0;
+    // 2) Adder 인스턴스
+    wire [15:0] sum_w;
+    Adder Ad1 (
+        .a(mult_w[7:0]),   // Multiplier 결과 하위 8비트
+        .b(in_b),
+        .sum(sum_w),
+        .cout()            // 캐리는 사용 안 함
+    );
 
-    // 1) 곱셈
-    Multiplier Mult1(.clk(clk), .rst(rst),.a(in_a), .b(in_b),.out(mult_result));
-
-    // 2) 곱셈 결과 + 이전 누적값
-    Adder Ad1(.a(mult_result),.b(q_out_c),.sum(out_sum),.cout());
-
-    // 3) 누적값 등록
-    FF_8bit FF3(.clk(clk), .rst(rst), .a(out_sum), .out(q_out_c));
-
-    // 4) 파이프라인 레지스터
-    FF_8bit FF1(.clk(clk), .rst(rst), .a(in_a), .out(out_a));
-    FF_8bit FF2(.clk(clk), .rst(rst), .a(in_b), .out(out_b));
-
-    // 5) 출력 연결
-    if(sel == 1'b0) assign out = out_b;
-    else assign out = q_out_c;
-
+    // 3) 출렵
+    //assign out_a    = in_a;
+    //assign out_down = sum_w[7:0];  // init*in_a + in_b 의 하위 8비트
+    always @ (posedge clk, posedge rst) begin
+        if (rst) begin
+            out_a    <= 8'b0;
+            out_down <= 8'b0;
+        end else begin
+            out_a    <= in_a;
+            out_down <= sum_w[7:0];  // init*in_a + in_b 의 하위 8비트
+        end
+    end
 endmodule
